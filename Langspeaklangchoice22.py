2541b84499524f777d192b059a71347c4a95e8a5
Harish Ravi <39822028+harishravi121@users.noreply.github.com>
2024-09-01 19:09:08 +0530

    Add files via upload

---
+++ Langspeaklangchoice22.py
@@ -0,0 +1,25 @@
+
+import random
+
+from google_speech import Speech
+from googletrans import Translator
+translator = Translator()
+
+srclang='en'
+destlang='zh-CN'
+destlangs=['kn','ta','te','hi','zh-CN','de']
+text1=''
+i=0
+
+a=['Maggie forty rupees','Brinjal thirty five rupees','Gold six thousand rupees','You are a good person']
+while(1):
+    i=i+1
+    a=['Maggie '+str(random.randint(25,80))+' rupees','Brinjal '+str(random.randint(25,80))+' rupees','Gold '+str(random.randint(2995,8990))+' rupees','You are a good person']
+    y=random.choice(a)
+    destlang=random.choice(destlangs)
+    print(y)
+    translated = translator.translate(y, src=srclang, dest=destlang)
+    print(translated.text)
+    speech = Speech(translated.text,destlang)
+    speech.play()
+
